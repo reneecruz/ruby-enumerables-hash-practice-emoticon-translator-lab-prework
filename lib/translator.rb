bbaf0c886cca7a424da474a78996ab4d8bed9180
Renée Cruz <reneebasscruz@gmail.com>
2019-08-05 02:08:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
  require  "pry"
  require "yaml"
+=======
+ require "yaml"
+ require "pry"
+>>>>>>> ef50fff305734f6332c7d753792b84c5228b6080
 
 def load_library(file_path)
   yaml_hash = YAML.load_file(file_path)
@@ -7,11 +12,17 @@ def load_library(file_path)
   
   yaml_hash.each do |key, value| 
   emoticon_hash[:get_emoticon][value[0]] = value[1]
+<<<<<<< HEAD
   emoticon_hash[:get_meaning][value[1]] = key
+=======
+        #binding.pry
+
+>>>>>>> ef50fff305734f6332c7d753792b84c5228b6080
   end
   emoticon_hash
 end
 
+<<<<<<< HEAD
 def get_japanese_emoticon(file_path, emoticon)
  load_library(file_path).each do |key, value|
     value.each do |data|
@@ -20,6 +31,11 @@ def get_japanese_emoticon(file_path, emoticon)
       end
     end
   end
+=======
+def get_japanese_emoticon
+  
+ 
+>>>>>>> ef50fff305734f6332c7d753792b84c5228b6080
 end
 
 def get_english_meaning(file_path, emoticon)
