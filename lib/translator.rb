65595ed1e71ff7e7a7839f375822b0cdbc47053f
Renée Cruz <reneebasscruz@gmail.com>
2019-08-05 02:11:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,12 +21,7 @@ def get_japanese_emoticon(file_path, emoticon)
       end
     end
   end
-  
-def get_japanese_emoticon
-  
- 
->>>>>>> ef50fff305734f6332c7d753792b84c5228b6080
-end
+
 
 def get_english_meaning(file_path, emoticon)
   emoticon_hash = load_library(file_path)
